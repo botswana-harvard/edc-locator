9d538c1ffd23f9450d028c245a1e654c1df024bb
ckgathi <ckgathi@gmail.com>
2019-04-11 19:28:18 +0200

    get reference_model model from app config

--- edc_locator/action_items.py
+++ edc_locator/action_items.py
@@ -1,12 +1,14 @@
+from django.apps import apps as django_apps
 from edc_action_item import Action, site_action_items, HIGH_PRIORITY
 
 SUBJECT_LOCATOR_ACTION = 'submit-subject-locator'
 
 
 class SubjectLocatorAction(Action):
+    app_config = django_apps.get_app_config('edc_locator')
     name = SUBJECT_LOCATOR_ACTION
     display_name = 'Submit Subject Locator'
-    reference_model = 'edc_locator.subjectlocator'
+    reference_model = app_config.reference_model
     show_link_to_changelist = True
     admin_site_name = 'edc_locator_admin'
     priority = HIGH_PRIORITY
--- edc_locator/apps.py
+++ edc_locator/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig as DjangoAppConfig
 
 class AppConfig(DjangoAppConfig):
     name = 'edc_locator'
+    reference_model = 'edc_locator.subjectlocator'
