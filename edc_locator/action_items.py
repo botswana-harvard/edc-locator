17e42fad36090d9398921ad77e3d382c341527c5
ckgathi <ckgathi@gmail.com>
2019-04-18 10:38:22 +0200

    change back loccater action item reference model

--- edc_locator/action_items.py
+++ edc_locator/action_items.py
@@ -1,14 +1,12 @@
-from django.apps import apps as django_apps
 from edc_action_item import Action, site_action_items, HIGH_PRIORITY
 
 SUBJECT_LOCATOR_ACTION = 'submit-subject-locator'
 
 
 class SubjectLocatorAction(Action):
-    app_config = django_apps.get_app_config('edc_locator')
     name = SUBJECT_LOCATOR_ACTION
     display_name = 'Submit Subject Locator'
-    reference_model = app_config.reference_model
+    reference_model = 'edc_locator.subjectlocator'
     show_link_to_changelist = True
     admin_site_name = 'edc_locator_admin'
     priority = HIGH_PRIORITY
--- edc_locator/apps.py
+++ edc_locator/apps.py
@@ -3,4 +3,3 @@ from django.apps import AppConfig as DjangoAppConfig
 
 class AppConfig(DjangoAppConfig):
     name = 'edc_locator'
-    reference_model = 'edc_locator.subjectlocator'
